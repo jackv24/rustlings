0e8a51234dd87208ff41eeb7ce54ef3de5a22c9a
Jack Vine <jackv949@gmail.com>
2020-11-19 16:30:13 +1030

    Completed all current exxercises!

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,24 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let parts: Vec<&str> = s.split(',').collect();
+        
+        if parts.len() < 2 {
+            return Person::default();
+        }
+
+        let name = parts[0];
+        if name.is_empty() {
+            return Person::default();
+        }
+
+        let result = parts[1].parse::<usize>();
+        match result {
+            Ok(val) => Person { name: String::from(name), age: val },
+            Err(_) => Person::default()
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Steps:
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
@@ -23,6 +22,24 @@ struct Person {
 impl FromStr for Person {
     type Err = String;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(String::from("error"));
+        }
+
+        let parts = s.split(',').collect::<Vec<_>>();
+        if parts.len() < 2 {
+            return Err(String::from("error"));
+        }
+
+        let name = parts[0];
+        if name.is_empty() {
+            return Err(String::from("error"));
+        }
+
+        match parts[1].parse::<usize>() {
+            Ok(age) => Ok(Person { name: String::from(name), age }),
+            Err(e) => Err(String::from("error"))
+        }
     }
 }
 
